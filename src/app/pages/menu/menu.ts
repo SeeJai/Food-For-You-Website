63bcf30bac1802321a8d2a872b00401a62736be3
Amal <amalahammedcj7@gmail.com>
2026-02-26 09:29:03 +0530

    Feat: Added Search and Sort in Menu

--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -4,23 +4,29 @@ import { ProductService } from '../../core/services/product.service';
 import { Product } from '../../core/models/product.model';
 import { Category } from '../../core/models/category.model';
 import { ProductCardComponent } from '../../shared/components/product-card/product-card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent],
+  imports: [CommonModule, ProductCardComponent, FormsModule],
   templateUrl: './menu.html',
   styleUrl: './menu.scss'
 })
 export class Menu {
 
   categories: Category[] = [];
+  allProducts: Product[] =[];
   products: Product[] = [];
+
   selectedCategoryId: number | null = null;
+  searchTerm: string = '';
+  sortOption: string = '';
 
   constructor(private productService: ProductService) {
     this.categories = this.productService.getCategories();
-    this.products = this.productService.getProducts();
+    this.allProducts = this.productService.getProducts();
+    this.products = [...this.allProducts];
   }
 
   get selectedCategory(): Category | undefined {
@@ -32,11 +38,35 @@ export class Menu {
 
   filterByCategory(categoryId: number | null) {
     this.selectedCategoryId = categoryId;
+    this.applyFilters();
+  }
 
-    if (categoryId === null) {
-      this.products = this.productService.getProducts();
-    } else {
-      this.products = this.productService.getProductsByCategory(categoryId);
-    }
+    applyFilters() {
+      let filtered = [...this.allProducts];
+
+      if (this.selectedCategoryId !== null) {
+        filtered = filtered.filter(
+          (p) => p.categoryId === this.selectedCategoryId);
+      }
+
+      if (this.searchTerm.trim()){
+        filtered = filtered.filter((p) =>
+          p.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+      }
+      
+      switch (this.sortOption) {
+        case 'priceLow':
+          filtered.sort((a,b) => a.price - b.price);
+          break;
+        case 'priceHigh':
+          filtered.sort((a,b) => b.price - a.price);
+          break;
+        case 'name':
+          filtered.sort((a,b) => a.name.localeCompare(b.name));
+          break;
+      }
+
+    this.products = filtered;
   }
 }
