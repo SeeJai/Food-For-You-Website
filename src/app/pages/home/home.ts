5b47d7b5ef23977c81e2dd0a69a2fdf46dd8ba3c
Amal <amalahammedcj7@gmail.com>
2026-02-26 10:24:00 +0530

    Added Featured Product section in Home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ProductService } from '../../core/services/product.service';
+import { Product } from '../../core/models/product.model';
+import { ProductCardComponent } from '../../shared/components/product-card/product-card';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterModule, ProductCardComponent],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrl: './home.scss'
 })
 export class Home {
 
+  featuredProducts: Product[] = [];
+
+  constructor(private productService: ProductService) {
+    const allProducts = this.productService.getProducts();
+
+    // Select products with tags (Bestseller / Seasonal etc)
+    this.featuredProducts = allProducts
+      .filter(p => p.tags && p.tags.length > 0)
+      .slice(0, 4);
+  }
 }
