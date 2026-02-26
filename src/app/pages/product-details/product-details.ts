80407434a4e7d1b94d92b76927237ccfc44d62ab
Amal <amalahammedcj7@gmail.com>
2026-02-26 10:47:25 +0530

    Product details Page UI

--- src/app/pages/product-details/product-details.ts
+++ src/app/pages/product-details/product-details.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../core/services/product.service';
+import { Product } from '../../core/models/product.model';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-details.html',
   styleUrl: './product-details.scss',
 })
 export class ProductDetails {
-
+  product?: Product;
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.product = this.productService.getProducts().find(p => p.id === id);
+  }
 }
