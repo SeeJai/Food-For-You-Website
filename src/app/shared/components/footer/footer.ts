bd0b8e0a9b73b01a1603bf8089d0da9f249a4671
Amal Ahammed CJ <amalahammedcj7@gmail.com>
2026-03-02 12:45:01 +0530

    Added Footer Filler Content

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
